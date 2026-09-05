2eaa1ec8c0823545e3fd4280f9f53d781d40ba63
吴彦祖 <3158896422@qq.com>
2026-09-05 21:26:57 +0800

    Revert "feat: 内置搜索新增酷我音乐(kw)和咪咕音乐(mg)平台支持"
    This reverts commit cba2a13d7adbd8f227a0494fc1b9c23d434eb663.

--- src/musicSdk/index.js
+++ src/musicSdk/index.js
@@ -6,16 +6,12 @@
 const kg = require('./kg');
 const tx = require('./tx');
 const wy = require('./wy');
-const kw = require('./kw');
-const mg = require('./mg');
 
 // 支持的音源列表
 const sources = [
   { name: '酷狗音乐', id: 'kg' },
   { name: 'QQ 音乐', id: 'tx' },
-  { name: '网易云音乐', id: 'wy' },
-  { name: '酷我音乐', id: 'kw' },
-  { name: '咪咕音乐', id: 'mg' }
+  { name: '网易云音乐', id: 'wy' }
 ];
 
 // 统一搜索接口
@@ -41,10 +37,6 @@ async function searchMusic(keyword, page = 1, limit = 30, source = 'all') {
       return tx.search(keyword, page, limit);
     case 'wy':
       return wy.search(keyword, page, limit);
-    case 'kw':
-      return kw.search(keyword, page, limit);
-    case 'mg':
-      return mg.search(keyword, page, limit);
     default:
       throw new Error('不支持的音源：' + source);
   }
@@ -90,10 +82,6 @@ async function getLyric(songInfo) {
       return tx.getLyric(songInfo);
     case 'wy':
       return wy.getLyric(songInfo);
-    case 'kw':
-      return kw.getLyric(songInfo);
-    case 'mg':
-      return mg.getLyric(songInfo);
     default:
       throw new Error('不支持的音源：' + source);
   }
